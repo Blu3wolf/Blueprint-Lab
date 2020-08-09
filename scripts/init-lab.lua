f9afa2ed347765ea50df0225314d09a7dd8ff2ce
daniel90c <daniel90c@gmail.com>
2020-08-09 14:32:43 -0300

    Revert " surface.generate_with_lab_tiles = true order"
    This reverts commit 979bea78cfd65a77900c6d9c4f39ea92b9d8075a.

--- scripts/init-lab.lua
+++ scripts/init-lab.lua
@@ -8,11 +8,10 @@ function InitLab(force)
         })
         
         surface.always_day = true
-       
+        surface.generate_with_lab_tiles = true
 
         ChunkLab(surface)
-        surface.generate_with_lab_tiles = true
-        --TileLab(surface)
+        TileLab(surface)
         EquipLab(surface, force)
     end
     
@@ -32,19 +31,19 @@ function ChunkLab(surface)
     end
 end
 
--- function TileLab(surface)
---     tiles = {}
---     for i = -LabRadius*32, LabRadius*32 - 1 do
---         for j = -LabRadius*32, LabRadius*32 - 1 do
---             if (i + j) % 2 == 0 then
---                 table.insert(tiles, {name = "lab-dark-1", position = {i, j}})
---             else
---                 table.insert(tiles, {name = "lab-dark-2", position = {i, j}})
---             end
---         end
---     end
---     surface.set_tiles(tiles)
--- end
+function TileLab(surface)
+    tiles = {}
+    for i = -LabRadius*32, LabRadius*32 - 1 do
+        for j = -LabRadius*32, LabRadius*32 - 1 do
+            if (i + j) % 2 == 0 then
+                table.insert(tiles, {name = "lab-dark-1", position = {i, j}})
+            else
+                table.insert(tiles, {name = "lab-dark-2", position = {i, j}})
+            end
+        end
+    end
+    surface.set_tiles(tiles)
+end
 
 function EquipLab(surface, force)
     electricInterface = surface.create_entity {name = "electric-energy-interface", position = {0, 0}, force = force}
