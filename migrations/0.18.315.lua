98e7564ce9d5d3deeead441a4ec64b9224aa1407
daniel90c <daniel90c@gmail.com>
2020-08-09 14:50:05 -0300

    Migrations 0.18.14 2

--- migrations/0.18.315.lua
+++ migrations/0.18.315.lua
@@ -12,17 +12,7 @@ for _, surface in pairs(game.surfaces) do
         surface.generate_with_lab_tiles = true
 
         ChunkLab(surface)
-        tiles = {}
-        for i = -LabRadius*32, LabRadius*32 - 1 do
-            for j = -LabRadius*32, LabRadius*32 - 1 do
-                if (i + j) % 2 == 0 then
-                    table.insert(tiles, {name = "lab-dark-1", position = {i, j}})
-                else
-                    table.insert(tiles, {name = "lab-dark-2", position = {i, j}})
-                end
-            end
-        end
-        surface.set_tiles(tiles)
+        TileLab(surface)
         EquipLab(surface, force)
         -- surface.map_gen_settings = {
     --     default_enable_all_autoplace_controls = false,
